a7e295d5605595d08693890708b87230d9d719e2
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-05-10 14:02:15 +0000

    アプリ作成開始

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Portfolio
+module Takuhure
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
