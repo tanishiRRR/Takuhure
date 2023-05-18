9eb556c4d11f0b12688d6c187edb5d2769d64e8c
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-05-18 11:33:19 +0000

    devise用のコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :end_users
+
+  # 会員用
+  # URL /customers/sign_in ...
+  devise_for :end_users, controller: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
