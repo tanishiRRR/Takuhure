f2f8713f199da44de1c79de4a7d2269331e3c724
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-05-18 14:03:58 +0000

    会員側routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,57 +1,32 @@
 Rails.application.routes.draw do
 
-  namespace :public do
-    get 'comments/index'
-    get 'comments/new'
-    get 'comments/create'
-    get 'comments/show'
-    get 'comments/destroy'
-  end
-  namespace :public do
-    get 'learning_records/index'
-    get 'learning_records/new'
-    get 'learning_records/create'
-    get 'learning_records/show'
-    get 'learning_records/edit'
-    get 'learning_records/update'
-    get 'learning_records/destroy'
-  end
-  namespace :public do
-    get 'answers/index'
-    get 'answers/new'
-    get 'answers/create'
-    get 'answers/show'
-    get 'answers/destroy'
-  end
-  namespace :public do
-    get 'supplemental_questions/create'
-    get 'supplemental_questions/new'
-    get 'supplemental_questions/destroy'
-  end
-  namespace :public do
-    get 'questions/create'
-    get 'questions/new'
-    get 'questions/destroy'
-    get 'questions/index'
-    get 'questions/top'
-    get 'questions/show'
-  end
-  namespace :public do
-    get 'end_users/show'
-    get 'end_users/edit'
-    get 'end_users/update'
-    get 'end_users/unsubscribe'
-    get 'end_users/withdraw'
-  end
-  namespace :public do
-    get 'homes/top'
-  end
   # 会員用
   # URL /customers/sign_in ...
   devise_for :end_users, controller: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+
+  scope :public do
+    root to: 'homes#top'
+
+    get   'end_users/my_page' => 'end_users#show'
+    get   'end_users/information/edit' => 'end_users#edit'
+    patch 'end_users/information' => 'end_users#update'
+    get   'end_users/unsubscribe' => 'end_users#unsubscribe'
+    patch 'end_users/withdraw' => 'end_users#withdraw'
+
+    get 'questions/top' => 'questions#top'
+    resources :questions, only: [:index, :create, :new, :show, :destroy]
+
+    resources :supplemental_questions, only: [:create, :new, :destroy]
+
+    resources :answers, only: [:index, :create, :new, :show, :destroy]
+
+    resources :learning_records
+
+    resources :comments, only: [:index, :create, :new, :show, :destroy]
+  end
 
   # 管理者用
   # URL /admin/sign_in ...
