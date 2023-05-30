afd87691c34f8ac314643a9e7cb3c2a32d136079
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-05-30 16:12:56 +0000

    管理者会員情報編集

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -1,10 +1,28 @@
 class Admin::EndUsersController < ApplicationController
+
+  before_action :authenticate_admin!
+
   def show
+    @end_user = EndUser.find(params[:id])
   end
 
   def edit
+    @end_user = EndUser.find(params[:id])
   end
 
   def update
+    @end_user = EndUser.find(params[:id])
+    if @end_user.update(end_user_params)
+      redirect_to admin_end_user_path(@end_user), success: '会員情報を編集しました'
+    else
+      render :edit
+    end
   end
+
+  private
+
+    def end_user_params
+      params.require(:end_user).permit(:account_name, :email, :is_study, :exam_date, :is_deleted)
+    end
+
 end
