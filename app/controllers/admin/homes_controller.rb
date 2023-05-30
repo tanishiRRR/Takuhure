354d1460e04cc13a039e4e160a22be1049442a53
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-05-30 15:31:01 +0000

    会員一覧

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,8 +1,9 @@
 class Admin::HomesController < ApplicationController
 
-  before_action :authenticate_end_user!
+  before_action :authenticate_admin!
 
   def top
+    @end_users = EndUser.all
   end
 
 end
