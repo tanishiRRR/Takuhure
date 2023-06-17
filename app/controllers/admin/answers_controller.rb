6dcb94f1e791cff8c555087ced7ffe2d4d795cbf
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-06-17 20:14:19 +0000

    管理者機能質問

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -1,5 +1,11 @@
 class Admin::AnswersController < ApplicationController
+
   def index
+    if params[:end_user_id].present?
+      @answers = Answer.where(end_user_id: params[:category_id]).order(created_at: :asc)
+    else
+      @answers = Answer.all.order(created_at: :asc)
+    end
   end
 
   def show
