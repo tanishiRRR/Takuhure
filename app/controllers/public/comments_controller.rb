4984fef46a8a819c5ced0ae6e8ab602bc92e921e
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-10-01 04:49:48 +0000

    コメント編集機能

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -25,9 +25,9 @@ class Public::CommentsController < ApplicationController
     end
   end
 
-  # def edit
-  #   @comment = Comment.find(params[:id])
-  # end
+  def edit
+    @comment = Comment.find(params[:id])
+  end
 
   # def update
   #   comment = Comment.find(params[:id])
